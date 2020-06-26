9aeadbac8f5373b208b5a1d96f452ae0f637c179
Costi Crîng <66464527+costy42006@users.noreply.github.com>
2020-06-26 10:50:02 +0300

    Add files via upload

--- site/js/mainjs.js
+++ site/js/mainjs.js
@@ -4,14 +4,13 @@ document.getElementById("cyear2").innerHTML = d.getFullYear();
 
 
 function myFunction() {
-    var x =
-        document.getElementById("myInput").value;
+    var x = document.getElementById("myInput").value;
     document.getElementById("myTD").innerHTML = x;
+    document.getElementById("myInput").value = "";
 }
 
 function myFunctionb() {
-    var y =
-        document.getElementById("myInputb").value;
-    console.log(y);
+    var y = document.getElementById("myInputb").value;
     document.getElementById("myTDb").innerHTML = y;
+    document.getElementById("myInputb").value = "";
 }
